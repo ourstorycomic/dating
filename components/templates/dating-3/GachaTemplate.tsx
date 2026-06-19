08ee9cda8d6c644200f0beb9051798d6e685f6d0
Zeus Gang Project <kaz442197@gmail.com>
2026-06-20 02:59:26 +0700

    fix: import useRef in GachaTemplate

--- components/templates/dating-3/GachaTemplate.tsx
+++ components/templates/dating-3/GachaTemplate.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { AnimatePresence } from "framer-motion";
 import confetti from "canvas-confetti";
 
