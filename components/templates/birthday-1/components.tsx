fea367458cdabeb949058b758d1d61cb81e5d15d
Zeus Gang Project <kaz442197@gmail.com>
2026-07-08 14:54:22 +0700

    Birthday-1: fix HangingItem centering by manually translating DOM element instead of relying on Html center

--- components/templates/birthday-1/components.tsx
+++ components/templates/birthday-1/components.tsx
@@ -2443,8 +2443,10 @@ function HangingItem({ x, startX, ropeLength, index, children, isLetter = false 
       </mesh>
 
       <group position={[0, -0.84, 0]}>
-        <Html transform center distanceFactor={2.8}>
-          {children}
+        <Html transform distanceFactor={2.8}>
+          <div style={{ transform: 'translate(-50%, -50%)', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            {children}
+          </div>
         </Html>
       </group>
     </group>
